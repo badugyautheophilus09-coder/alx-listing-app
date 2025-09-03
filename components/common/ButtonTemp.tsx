d5a99b27ff342a228e76e3396947e31f20e7c4fd
badugyautheophilus09-coder <badugyautheophilus09@gmail.com>
2025-09-03 15:39:20 +0000

    Rename Button component to temp

---
+++ components/common/ButtonTemp.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface ButtonProps {
+  onClick?: () => void;
+  children: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children, type = 'button' }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
